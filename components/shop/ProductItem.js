709fe6cf5a4ad0cdd3fcdd6c1e6a5cd962a4e4fa
Ariene Maiara <ariene.maiara@hotmail.com>
2020-03-08 20:05:32 -0300

    Styling product items

---
+++ components/shop/ProductItem.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { 
+    View,
+    Text,
+    Image,
+    Button,
+    StyleSheet
+} from 'react-native';
+
+import Colors from '../../constants/Colors';
+
+const ProductItem = props => {
+    return (
+        <View style={styles.product}>
+            <Image
+                style={styles.image} 
+                source={{uri: props.imageUrl}} />
+            <View style={styles.details} >
+                <Text style={styles.title}>{props.title}</Text>
+                <Text style={styles.price}>${props.price.toFixed(2)}</Text>
+            </View>
+            <View style={styles.actions}>
+                <Button 
+                    color={Colors.primary}
+                    title="View Details"
+                    onPress={props.onViewDetails} />
+                <Button 
+                    color={Colors.secondary}
+                    title="Add to Cart"
+                    onPress={props.onAddToCart} />
+            </View>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    product: {
+        shadowColor: 'black',
+        shadowOpacity: 0.26,
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 8,
+        elevation: 5,
+        borderRadius: 10,
+        backgroundColor: 'white',
+        height: 300,
+        margin: 20,
+        overflow: 'hidden'
+    },
+    image: {
+        width: '100%',
+        height: '65%'
+    },
+    details: {
+        alignItems: 'center',
+        height: '15%',
+        padding: 10
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        marginVertical: 4
+    },
+    price: {
+        fontSize: 14,
+        color: '#888'
+    },
+    actions: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        height: '25%',
+        paddingHorizontal: 20
+    }
+});
+
+export default ProductItem;
--- constants/Colors.js
+++ constants/Colors.js
@@ -1,4 +1,4 @@
 export default {
     primary: '#3f51b5',
-    secondary: '#51b53f'
+    secondary: '#b53f51'
 }
